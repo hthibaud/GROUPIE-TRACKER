cf41017e3d20fe487cb0e2209bf6e8084106ce7a
caromain <romain.carrere@ynov.com>
2025-12-10 17:05:00 +0100

    feat(cmd/main): add main.go

---
+++ cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	??? "???"
+)
+
+func main() {
+	handlers.Handler()
+	log.Println("Server Start on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
