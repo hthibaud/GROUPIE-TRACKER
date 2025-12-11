bf509bf9c4eb1606afc4e75ab1bdb7c9413142fd
caromain <romain.carrere@ynov.com>
2025-12-11 16:33:41 +0100

    feat(feat/handlers): add new branch "feat/handlers" and internal/handlers

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,10 +3,11 @@ package main
 import (
     "log"
     "net/http"
+
+    handlers "GROUPIE-TRACKER/internal/handlers"
 )
 
 func main() {
-	http.HandleFunc("/", Home)
     handlers.Handler()
     log.Println("Server Start on http://localhost:8080")
     http.ListenAndServe(":8080", nil)
