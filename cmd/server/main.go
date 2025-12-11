8265d7b7f2d8db29686970e3a868a6fc7c62e9a2
hthibaud <thibaud.herry@ynov.com>
2025-12-11 14:27:45 +0100

    feat(main): created a dev branch with a main.go

---
+++ cmd/server/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "log"
+    "net/http"
+)
+
+func main() {
+    handlers.Handler()
+    log.Println("Server Start on http://localhost:8080")
+    http.ListenAndServe(":8080", nil)
+}
