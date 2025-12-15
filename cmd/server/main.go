773bbe490a970dd5f092259b7aad51f5c9b9d4a7
caromain <romain.carrere@ynov.com>
2025-12-15 17:07:58 +0100

    feat(dev): add api

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
+    "io"
 
 	handlers "GROUPIE-TRACKER/internal/handlers"
 )
 
 func main() {
+
+    resp, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+	if err != nil {
+		fmt.Println("Erreur :", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Statut HTTP :", resp.Status)
+
+    body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Erreur lecture :", err)
+		return
+	}
+
+	fmt.Println(string(body))
+
 	handlers.Handler()
-	log.Println("Server Start on http://localhost:8080")
+	fmt.Println("Server Start on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
