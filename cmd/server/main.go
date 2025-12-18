faa8ce8c6af2cf020ff8ac7bde2d806a69445b5a
hthibaud <thibaud.herry@ynov.com>
2025-12-18 16:02:26 +0100

    feat(api): moved the API code from main to api.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,29 +3,11 @@ package main
 import (
 	"fmt"
 	"net/http"
-    "io"
 
 	handlers "GROUPIE-TRACKER/internal/handlers"
 )
 
 func main() {
-
-    resp, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
-	if err != nil {
-		fmt.Println("Erreur :", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	fmt.Println("Statut HTTP :", resp.Status)
-
-    body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Erreur lecture :", err)
-		return
-	}
-
-	fmt.Println(string(body))
 
 	handlers.Handler()
 	fmt.Println("Server Start on http://localhost:8080")
--- internal/api/api.go
+++ internal/api/api.go
@@ -1,7 +1,25 @@
 func getApi(w http.ResponseWriter, r *http.Request) {
-    if r.Method != "GET" {
-        http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-        return
-    }
-    fmt.Fprintf(w, "Voici les items disponibles.")
+	if r.Method != "GET" {
+
+		resp, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+		if err != nil {
+			fmt.Println("Erreur :", err)
+			return
+		}
+		defer resp.Body.Close()
+
+		fmt.Println("Statut HTTP :", resp.Status)
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("Erreur lecture :", err)
+			return
+		}
+
+		fmt.Println(string(body))
+
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Fprintf(w, "Voici les items disponibles.")
 }
