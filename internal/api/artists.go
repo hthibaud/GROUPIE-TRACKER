23adfaa1a4a69dfa237fcca2f665f54079faafe6
hthibaud <thibaud.herry@ynov.com>
2025-12-18 16:58:42 +0100

    feat(api): added the artists struct

--- internal/api/artists.go
+++ internal/api/artists.go
@@ -5,6 +5,18 @@ import (
 	"net/http"
 )
 
+type Artist struct {
+	ID           int      `json:"id"`
+	Name         string   `json:"name"`
+	Image        string   `json:"image"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Locations    string   `json:"locations"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
+}
+
 func getArtists([]Artist, error) {
 	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
 
--- internal/api/client.go
+++ internal/api/client.go
@@ -1,12 +1,1 @@
 package client
-
-type artists struct {
-	id             int
-	name           string
-	picture        string
-	dateOfCreation string
-	firstAlbum     string
-	members        []string
-	place          string
-	showsDates     string
-}
